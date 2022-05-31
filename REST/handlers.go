7ee7536fc3c8e5d34a01b6e06a1305a1e5f2a081
khaifazil <khairal.fazil@gmail.com>
2022-05-31 20:00:39 +0800

    initial commit

---
+++ REST/handlers.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the REST API!")
+}
+
+func allCourses(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "List of all courses")
+
+	//returns the key/value pairs in the query string as a map object
+	kv := r.URL.Query()
+
+	for k, v := range kv {
+		fmt.Println(k, v) //print out the key/value pair
+	}
+	// returns all the courses in JSON
+	json.NewEncoder(w).Encode(courses)
+}
+
+func course(w http.ResponseWriter, r *http.Request) {
+
+	if !validKey(r) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("401 - Invalid key"))
+		return
+	}
+
+	params := mux.Vars(r)
+	// fmt.Fprintf(w, "Detail for course "+params["courseid"])
+	// fmt.Fprintf(w, "\n")
+	// fmt.Fprintf(w, r.Method)
+
+	if r.Method == "GET" {
+		if _, ok := courses[params["courseid"]]; ok {
+			json.NewEncoder(w).Encode(courses[params["courseid"]])
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No course found"))
+		}
+	}
+
+	if r.Method == "DELETE" {
+		if _, ok := courses[params["courseid"]]; ok {
+			delete(courses, params["courseid"])
+			w.WriteHeader(http.StatusAccepted)
+			w.Write([]byte("202 - Course deleted: " + params["courseid"]))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No course found"))
+		}
+	}
+
+	if r.Header.Get("Content-type") == "application/json" {
+
+		//POST is for creating new course
+		if r.Method == "POST" {
+
+			//read the string sent to the service
+			var newCourse courseInfo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close()
+
+			if err == nil {
+				//convert JSON to object
+				json.Unmarshal(reqBody, &newCourse)
+
+				if newCourse.Title == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 - Please supply course " + "information " + "in JSON format"))
+					return
+				}
+
+				//check if course exists; add only if course does not exists
+				if _, ok := courses[params["courseid"]]; !ok {
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("201 - Course added: " + params["courseid"]))
+				} else {
+					w.WriteHeader(http.StatusConflict)
+					w.Write([]byte("409 - Duplicate course ID"))
+				}
+			} else {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				w.Write([]byte("422 - Please supply course information " + "in JSON format"))
+			}
+		} // end of post for new course
+
+		//---PUT is for creating or updating existing course---
+		if r.Method == "PUT" {
+			var newCourse courseInfo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				json.Unmarshal(reqBody, &newCourse)
+				if newCourse.Title == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 - Please supply course " + " information " + "in JSON format"))
+					return
+				}
+
+				// check if course exists; add only if course does not exist
+				if _, ok := courses[params["courseid"]]; !ok {
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("201 - Course added: " + params["courseid"]))
+				} else {
+					// update course
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusAccepted)
+					w.Write([]byte("202 - Course updated: " + params["courseid"]))
+				}
+			} else {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				w.Write([]byte("422 - Please supply " + "course information " + "in JSON format"))
+			}
+		} // end of put for update
+	}
+}
---
+++ REST/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type courseInfo struct {
+	Id    int
+	Title string `json:"Title"`
+}
+
+// used for storing courses on the REST API
+var courses map[string]courseInfo
+
+func validKey(r *http.Request) bool {
+	v := r.URL.Query()
+	if key, ok := v["key"]; ok {
+		if key[0] == "2c78afaf-97da-4816-bbee-9ad239abb296" {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+func main() {
+	// instantiate courses
+	courses = make(map[string]courseInfo)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/v1/", home)
+	router.HandleFunc("/api/v1/courses", allCourses)
+	router.HandleFunc("/api/v1/courses/{courseid}", course).Methods("GET", "PUT", "POST", "DELETE")
+
+	fmt.Println("Listening at port 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
